916275cdc3a4c438fe22842441b86f2341ddaa29
SBrayanAlves <sbrayanalves@gmail.com>
2025-12-18 21:26:18 -0300

    users - Complete User Models

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -129,4 +129,6 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+AUTH_USER_MODEL = "users.Profile"
+
 CORS_ALLOW_CREDENTIALS = True
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,65 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.text import slugify
 
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    email = models.EmailField(unique=True)
+    profile_picture = models.ImageField(upload_to="profile_pictures/", blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, unique=True, blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+    
+    def __str__(self):
+        return self.username
+    
+    def save(self, *args, **kwargs):
+
+        if not self.slug:
+            base = slugify(self.username)
+            self.slug = f"{base}-{str(self.id)[:8]}"
+
+        super().save(*args, **kwargs)
